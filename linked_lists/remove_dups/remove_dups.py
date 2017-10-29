6443437b3f5799b824f0d509a90a57573657cef9
Jason Ganub <jasonganub@gmail.com>
2017-10-28 17:47:35 -0700

    Renamed all directories because Python hates any directories or modules that start with a number -_-.

---
+++ linked_lists/remove_dups/remove_dups.py
@@ -0,0 +1,25 @@
+folder = __import__('linked_lists')
+
+
+def remove_dups(node):
+    nodes = {}
+
+    # make current head
+
+    # check if the node data is in the hash
+    # if it is, remove it so keep track of current and current.next
+    # if it's not, add it to hash
+
+    # return head
+    return "YAY"
+
+ll = LinkedList()
+ll.generate(100, 0, 9)
+print(ll)
+remove_dups(ll)
+print(ll)
+
+ll.generate(100, 0, 9)
+print(ll)
+remove_dups(ll)
+print(ll)
